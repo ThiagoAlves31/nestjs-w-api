2e3236199ce2a298d80eec6d6412f3b2dfd828cd
Thiago Alves <7564640-thiago_alves@users.noreply.gitlab.com>
2021-03-29 02:41:32 -0300

    update port to mysql

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -4,7 +4,7 @@ require('dotenv').config();
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
+  port: 3306,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
